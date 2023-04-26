c6dedb10516215daa4300f3e3f552cdff5c15773
Hoperaisers <abdallahsafari@gmail.com>
2023-04-26 16:20:01 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,120 @@
+// Variable Declaration
+// let firstName = "John"
+// let secondName = "Opiyo"
+// let age = 20;
+// let isStudent = true
+// let fruit = ["Apple", "Mango", "Banana", "Orange"]
+
+// let person = {
+//     name: "Joy",
+//     age: 30,let i =1;
+// let i = 1
+// do{
+//     console.log(i)
+//     ++i
+// }while(i<=5);
+
+//     isStudent: true
+// }
+// console.log(typeof firstName);
+// console.log(age++)
+// let num1 = 20
+// let num2 = 5
+
+// console.log(num1 + num2)
+// console.log(num1 * num2)
+// console.log(num1 / num2)
+// console.log(num1 % num2)
+// console.log(num1 - num2)
+
+// {
+// Compasison Operator
+// == 	equals to~ returns true if 2 values are equal
+// let x = 6
+// let y = 5
+// console.log(x == y)
+//  === 	equal value and equal type
+// console.log(x === y)
+
+// != 	not equal-returns true
+// console.log(x != y)
+
+// !== 	not equal value or not equal type
+// console.log(x !== y)
+
+// > 	greater than~ returns true if first value is greater than 2nd value
+// console.log(x > y)
+
+// < 	less than
+// console.log(x < y)
+
+// >= 	greater than or equal to
+// console.log(x >= y)
+
+// <= 	less than or equal to
+// console.log(x <= y)
+
+// }
+
+// let width = 20
+// let length = 50
+// let area = width * length
+// console.log(width * length)
+
+// let base = 5
+// let height = 10
+// let trianglearea = base * height
+// console.log(1 / 2 * base * height)
+
+// const Person1Age = 25;
+// const Person2Age = 30;
+// if (Person1Age > Person2Age) {
+//     console.log("Person 1 is older than Person 2");
+// } else if (Person2Age > Person1Age) {
+//     console.log("Person 2 is older than Person 1");
+// } else {
+//     console.log("Person 1 is older than Person 2");
+// }
+
+
+// for loop -- typically used,how many you want to repeat a block of codes.
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// while loop -- used when repeating a block of code when a certain condition is true.
+// let i = 2;
+// while (i <= 10) {
+//     console.log(i)
+//     i += 2
+// }
+
+// do while -- executes block of code atleast once before checking the condition
+// let i =1;
+// do{
+//     console.log(i)
+//     i++
+// }while(i<=5);
+
+// Switch Statement is a control flow statement that allows to test a value against a sries of cases and execute different code blocks depending on the value
+let dayOfWeek="Monday"
+switch
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
